d2dd9561a860ae24d1b72ffbba2ae6a29a9ddd15
yugo uchiida <y.uchiida@nirneege.com>
2022-09-24 18:47:52 +0900

    admins テーブルのマイグレーションファイルを作成 以下のコマンドを実行 - vendor/bin/sail artisan make:migration create_admins_table

---
+++ database/migrations/2022_09_24_064120_create_admins_table.php
@@ -0,0 +1,36 @@
+<?php
+
+/* 管理者ユーザーの情報を保持するテーブル admins を生成する
+ * 以下のコマンドでひな形を生成
+ * vendor/bin/sail artisan make:migration create_admins_table
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
