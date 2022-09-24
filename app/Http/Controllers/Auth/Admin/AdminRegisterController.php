a2e7e16b4dbbf2f1ae4d048b37f383aec5bf9b7a
yugo uchiida <y.uchiida@nirneege.com>
2022-09-24 19:03:48 +0900

    管理者ユーザー登録用のControllerファイルを作成 以下のコマンドを実行 - vendor/bin/sail artisan make:controller Auth/Admin/AdminRegisterController

---
+++ app/Http/Controllers/Auth/Admin/AdminRegisterController.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 管理者ユーザーの追加処理を担うController
+ * 以下のコマンドでひな形を生成
+ * vendor/bin/sail artisan make:controller Auth/Admin/AdminRegisterController
+ */
+
+namespace App\Http\Controllers\Auth\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminRegisterController extends Controller
+{
+    //
+}
